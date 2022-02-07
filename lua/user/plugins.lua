e7a7a62b794d55a643f69692650fffcc506dba24
Kyle Chuang <kchuangk@gmail.com>
2022-02-07 13:15:34 -0500

    added lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,14 @@ return packer.startup(function(use)
   -- Vim-slime
   use 'kchuangk/vim-slime'
 
+  -- Lualine
+  use { 'nvim-lualine/lualine.nvim', 
+        requires = {
+          'kyazdani42/nvim-web-devicons', opt=true-- optional, for file icon
+        },
+  }
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
